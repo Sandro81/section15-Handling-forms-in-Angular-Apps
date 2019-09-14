ad1a417fb9964caa674536aba2c2a732e6b28b41
Sandro81 <sandro.fioravanti81@gmail.com>
2019-09-14 21:25:58 +0100

    section 15

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,8 +10,16 @@ export class AppComponent {
   @ViewChild('f', {static: false}) signupForm: NgForm;
   defaultQuestion = 'pet';
   answer = '';
-
+  user = {
+    username: '',
+    email: '',
+    secretQuestion: '',
+    answer: '',
+    gender: ''
+  }
   genders = ['male', 'female'];
+  submitted = false;
+
   suggestUserName() {
     const suggestedName = 'Superuser';
    /* this.signupForm.setValue({
@@ -32,7 +40,14 @@ export class AppComponent {
   }
 
   onSubmit() {
-    console.log(this.signupForm);
+    this.submitted = true;
+    this.user.username = this.signupForm.value.userData.username;
+    this.user.email = this.signupForm.value.userData.email;
+    this.user.secretQuestion = this.signupForm.value.secret;
+    this.user.answer = this.signupForm.value.questionAnswer;
+    this.user.gender = this.signupForm.value.gender;
+
+    this.signupForm.reset();
   }
 
 }
